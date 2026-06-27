606087d17ae2eb8db93ea1e975a1e1b2211de093
Vidishaa Deo <vidishaa2709deo@gmail.com>
2026-06-27 14:23:39 +0530

    feat: add POST and GET API endpoints for magazines

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+magazines = []
 
 @app.route("/")
 def home():
@@ -10,11 +12,24 @@ def home():
         "message": "MemoryLane API Running 🚀"
     }
 
-@app.route("/magazines")
-def magazines():
+@app.route("/magazines", methods=["GET"])
+def get_magazines():
+
     return {
-        "magazines": []
+        "magazines": magazines
     }
+
+@app.route("/magazines", methods=["POST"])
+def create_magazine():
+
+    data = request.get_json()
+
+    magazines.append(data)
+
+    return {
+        "message": "Magazine created successfully!",
+        "magazine": data
+    }, 201
 
 @app.route("/pages")
 def pages():
